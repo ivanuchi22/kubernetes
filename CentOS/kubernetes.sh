01a056060dcdd8d28f89322183ee4e9969b0588a
Iván Peláez Sanz <142797916+ivanuchi22@users.noreply.github.com>
2024-05-22 13:19:10 +0200

    Create kubernetes.sh

---
+++ CentOS/kubernetes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+yum install yum-utils google-chrome-stable -y
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum remove runc -y
+yum install -y docker-ce docker-ce-cli containerd.io -y
+systemctl start docker
+systemctl enable docker
+yum-config-manager --add-repo https://raw.githubusercontent.com/ivanuchi22/kubernetes/main/CentOS/kubernetes.repo
+yum update
+systemctl enable --now kubelet
+mkdir -p /etc/containerd
+containerd config default | tee /etc/containerd/config.toml
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+systemctl restart containerd
+systemctl enable containerd
